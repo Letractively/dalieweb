96ccdd77b2824ad3d3e81be048cd3f74fc5e9b81
island.sommer@gmx.de <island.sommer@gmx.de@0d73a630-af78-11de-9d1a-45ab25924541>
2009-10-19 19:50:18 +0000

    Initial import.

---
+++ trunk/dalieweb/JavaSource/selinas/UploadApplication.java
@@ -0,0 +1,57 @@
+/*
+ * UploadApplication.java
+ * @author dv0101 on 23.03.2009 um 17:22:15
+ *
+ */
+package selinas;
+
+import org.apache.commons.fileupload.FileItem;
+/**
+ * <b>Class</b>UploadApplication:<br>check the UploadApplication<br>
+ * 
+ * @version 1.00 on 23.03.2009 um 08:22:15
+ * @author dv0101
+ * <br><br>
+ */
+public class UploadApplication {
+    /* Check the Choise Upload */
+    
+    /** 
+     * <B>name=</B>C:\Dokumente und Einstellungen\DV0101\Eigene Dateien\Eigene eBooks\hms020719mu.pdf,<BR> 
+     * <B>StoreLocation=</B>C:\Pfad\temp\Upload.tmp, <BR>
+     * <B>size=</B>201177bytes, <BR>
+     * <B>isFormField=</B>false, <br>
+     * <B>FieldName=</B>myFile <br>
+     * */
+    FileItem element;
+    /** Typ of Element<BR>Datei ist .pdf or .doc or .xls  */
+    private String application = "";//Typ of Element
+    
+    public UploadApplication(FileItem element){
+        this.element = element;
+    }//UploadApplication
+    
+    /** <B>vaild is:</B><BR>msword<BR>ms-excel<BR>pdf */
+    public boolean checkUplaodApplikation() {
+        if(element.getContentType().equalsIgnoreCase("application/msword")){
+      		this.application = ".doc";
+      		return true;
+        }else if (element.getContentType().equalsIgnoreCase("application/vnd.ms-excel")) {
+          	this.application = ".xls";
+          	return true;   
+        }else if (element.getContentType().equalsIgnoreCase("application/pdf")) {
+          	this.application = ".pdf";
+          	return true;
+        }//endif
+        return false;
+    }//checkUploadApplikation
+    
+    
+    /**
+     * @return Returns the String of application.
+     * <br> ".pdf or .xls or .doc
+     */
+    public String getApplication() {
+        return application;//Typ of Element
+    }//getApplication
+}//class UploadApplication
