ade3e8750309f04a6991454c0060088b73b82390
island.sommer@gmx.de <island.sommer@gmx.de@0d73a630-af78-11de-9d1a-45ab25924541>
2009-11-10 18:56:45 +0000

    Initial import.

---
+++ trunk/dalieweb/JavaSource/selinas/servlet/GoToStartServlet.java
@@ -0,0 +1,55 @@
+package selinas.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.Servlet;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GoToStartServlet extends HttpServlet implements Servlet {
+	
+	 /** perform for both HTTP <code>GET</code> and <code>POST</code> methods  */
+	protected void perForm(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+	    
+		String nextPage = "/selinas/selinas001.jsp";
+		
+ 		try {
+ 	        RequestDispatcher displogin =  request.getRequestDispatcher("LogOnCheck");
+      		displogin.include(request, response);
+      		
+ 		}catch (ServletException se) {
+ 		   performForward(nextPage,request,response);//Login 
+     	}//catch ServletException
+ 		performForward("/selinas/selinas002.jsp", request, response);
+	}//perForm
+    
+	/** handle the HTTP <code>GET</code> method */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+	    perForm(request,response);
+	}//doGet
+	/** handle the HTTP <code>POST</code> method */	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    perForm(request,response);
+	}//doPost
+	
+	/** handle the next step */
+	protected void performForward(String destination,HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {	
+		ServletContext context = getServletContext();
+		RequestDispatcher dispatcher = context.getRequestDispatcher(destination);
+		dispatcher.forward(request, response);
+	}//performForward
+	
+	public GoToStartServlet() {
+		super();
+	}//GoToStartServlet
+	
+	/**@return a short description of this Servlet */
+	public String getServletInfo() {
+	    return "navigation of Selinas";
+	}//getServletInfo
+
+}//class GoToStartServlet
