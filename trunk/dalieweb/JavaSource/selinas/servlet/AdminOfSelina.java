8e09c0a6eb0a6a29940b0a1ab5fa4ddacc1ec04e
island.sommer@gmx.de <island.sommer@gmx.de@0d73a630-af78-11de-9d1a-45ab25924541>
2009-11-28 14:09:24 +0000

    Initial import.

---
+++ trunk/dalieweb/JavaSource/selinas/servlet/AdminOfSelina.java
@@ -0,0 +1,74 @@
+package selinas.servlet;
+
+import help.LoggerHelper;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.Servlet;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import selinas.SelinasUser;
+import selinas.bean.SelinasSession;
+import database.Database;
+import database.dateien.Selinas;
+
+public class AdminOfSelina extends HttpServlet implements Servlet {
+	
+	 /** perform for both HTTP <code>GET</code> and <code>POST</code> methods  */
+	protected void perForm(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+	    
+		HttpSession session = request.getSession();
+		String nextPage = "/selinas/selinas001.jsp";
+		SelinasSession show = new SelinasSession((Selinas) session.getAttribute("Selinas")); 
+		SelinasUser selinasuser = (SelinasUser) session.getAttribute("User");
+	    Database dbConn = (Database) session.getAttribute("Database");
+		
+ 		try {
+ 	        RequestDispatcher displogin =  request.getRequestDispatcher("LogOnCheck");
+      		displogin.include(request, response);  		
+ 		}catch (ServletException se) {
+ 		   performForward(nextPage,request,response);//Login 
+     	}//catch ServletException
+ 		
+ 		try{
+     		session.setAttribute("Adresse", show.getAdressOfDatabase(dbConn,selinasuser.user));//SessionAttribut:AdressOfDatabase
+     		performForward("/selinas/selinas005.jsp", request, response);
+     	}catch (Exception e) {
+     		LoggerHelper.log(this.getClass().getName(), "Exception of perForm getAdressOfDatabase", e);
+	        performForward(nextPage,request,response);//Login 
+	     }//catch Exception
+ 		
+	}//perForm
+    
+	/** handle the HTTP <code>GET</code> method */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+	    perForm(request,response);
+	}//doGet
+	/** handle the HTTP <code>POST</code> method */	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    perForm(request,response);
+	}//doPost
+	
+	/** handle the next step */
+	protected void performForward(String destination,HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {	
+		ServletContext context = getServletContext();
+		RequestDispatcher dispatcher = context.getRequestDispatcher(destination);
+		dispatcher.forward(request, response);
+	}//performForward
+	
+	public AdminOfSelina() {
+		super();
+	}//GoToStartServlet
+	
+	/**@return a short description of this Servlet */
+	public String getServletInfo() {
+	    return "navigation of Selinas";
+	}//getServletInfo
+
+}//class AdminOfSelina
---
+++ trunk/dalieweb/JavaSource/selinas/servlet/AdressToProcessServlet.java
@@ -0,0 +1,102 @@
+package selinas.servlet;
+
+import help.LoggerHelper;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.Servlet;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import selinas.SelinasUser;
+import selinas.bean.SelinasSession;
+import database.Database;
+import database.dateien.Adresse;
+import database.dateien.Selinas;
+
+public class AdressToProcessServlet extends HttpServlet implements Servlet {
+	
+	/** perform for both HTTP <code>GET</code> and <code>POST</code> methods  */
+	protected void perForm(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+	    String nextPage = "/selinas/selinas001.jsp";
+	    SelinasSession show = new SelinasSession((Selinas) session.getAttribute("Selinas"));
+	    SelinasUser selinasuser = (SelinasUser) session.getAttribute("User"); 
+	    Adresse adressOfSession = (Adresse)session.getAttribute("Adresse");
+	    Database dbConn = (Database) session.getAttribute("Database");
+	     
+		try{
+		    RequestDispatcher displogin =  request.getRequestDispatcher("LogOnCheck");
+	    	displogin.include(request, response);
+		}catch (ServletException se) {
+		    performForward(nextPage,request,response);//Login 
+	     }//catch ServletException
+		 
+		 try{
+		 	RequestDispatcher errorServlet =  request.getRequestDispatcher("ErrorServlet");
+		 	errorServlet.include(request, response);
+		 }catch (ServletException se) {
+		 	performForward(nextPage,request,response);//Login 
+		 }//catch ServletException
+	     
+	 	 String error = (String) session.getAttribute("Error");
+	 	 if(request.getParameter("beenden") == null){//button beendet
+	 	 	try{
+	 	 		if(error.equalsIgnoreCase("yes")) { 
+	 	 			session.setAttribute("Adresse", show.getAdressOfUpdate(dbConn,selinasuser.user,adressOfSession,request));//SessionAttribut:AdressOfUpdate
+	 	 			performForward("/selinas/selinas005.jsp",request,response);
+	 	 		}else{
+	 	 			session.setAttribute("Adresse", show.getAdressOfUpdate(dbConn,selinasuser.user,adressOfSession,request));//SessionAttribut:AdressOfUpdate
+	 	 			performForward("/selinas/selinas005.jsp",request,response);
+	 	 		}//endif error.equals
+	 	 	}catch (Exception e) {
+	 	 		LoggerHelper.log(this.getClass().getName(), "Exception of perForm Verarbeitung", e);
+	 	 		performForward(nextPage,request,response);//Login 
+	 	 	}//catch ServletException
+	 	 }else{//button beendet
+ 	 		try {
+ 	 			if(error.equalsIgnoreCase("yes")) { 
+					session.setAttribute("Adresse", show.getAdressOfUpdate(dbConn,selinasuser.user,adressOfSession,request));//SessionAttribut:AdressOfUpdate
+					performForward("/selinas/selinas005.jsp",request,response);
+ 	 			}else{
+ 	 				session.setAttribute("Adresse", show.getAdressOfDatabase(dbConn,selinasuser.user));//SessionAttribut:AdressOfUpdate
+ 	 	 			performForward("/selinas/selinas002.jsp",request,response);
+ 	 			}//endif error.equals
+			} catch (Exception e) {
+				LoggerHelper.log(this.getClass().getName(), "Exception of perForm Beenden", e);
+				performForward(nextPage,request,response);//Login 
+			}//
+	 	 }//endif
+	}//perForm
+	
+	/** handle the HTTP <code>GET</code> method */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+	    perForm(request,response);
+	}//doGet
+	/** handle the HTTP <code>POST</code> method */	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    perForm(request,response);
+	}//doPost
+	/** handle the next step */
+	protected void performForward(String destination,HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {	
+		ServletContext context = getServletContext();
+		RequestDispatcher dispatcher = context.getRequestDispatcher(destination);
+		dispatcher.forward(request, response);
+	}//performForward
+	
+	public AdressToProcessServlet() {
+		super();
+	}//AdressServlet
+	
+	/**@return a short description of this Servlet */
+	public String getServletInfo() {
+	    return "work with UserBean Selinas";
+	}//getServletInfo
+
+
+}//class AdressToProcessServlet
