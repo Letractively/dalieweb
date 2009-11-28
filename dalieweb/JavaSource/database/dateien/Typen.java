a047e80e1348d350cf616912f9f73966f696953b
island.sommer <island.sommer@gmx.de>
2009-11-28 14:08:02 +0000

    Initial import.

---
+++ dalieweb/JavaSource/database/dateien/Typen.java
@@ -0,0 +1,125 @@
+/*
+ * Created of DV0101 on 16.11.2009 12:26:57
+ *
+ * Filename	  Typen.java
+ */
+package database.dateien;
+
+import help.HelpDate;
+
+import java.util.Vector;
+
+/**
+ * Class description goes here.
+ *
+ * @version 	1.00 16.11.2009 um 12:26:57
+ * @author 		DV0101
+ * <br><br>
+ * private   = "false" nur sichtbar innerhalb der Klasse<br>
+ * ohne      = 	       sichtbar innerhalb Package<br>
+ * protected = "false" sichtbar innerhalb Package und abgeleiteten Klassen<br>
+ * public    = "false" sichtbar ausserhalb<br>
+ */
+public class Typen {
+
+	private String typ;//AA
+    private String description;// Arbeitsanweisung
+
+    /** Kennzeichen des User der Typ erstellt hat */
+    private String createUserId;//DV0101 AnmeldeKennnung am System
+    /** Timestamp der Erstellung */
+    private String createDate;//Datum der Erstellung
+    /** Kennzeichen des User der letzten Aenderung */
+    private String changeUserId;//DV0101 AnmeldeKennnung am System
+    /** Timestamp der letzten Aenderung */
+    private String changeDate;//Datum der letzten Änderung
+    
+    
+    public Typen() {
+    }//Typ
+    public Typen(Vector columns) throws Exception {       
+        this.typ = columns.elementAt(0).toString();
+        this.description = columns.elementAt(1).toString();
+        
+        this.createUserId = columns.elementAt(2).toString();
+        this.createDate = HelpDate.getTT_MM_JJJJ_HHMMSSDB(columns.elementAt(3).toString());
+        this.changeUserId = columns.elementAt(4).toString();
+        this.changeDate = HelpDate.getTT_MM_JJJJ_HHMMSSDB(columns.elementAt(5).toString());
+    }//Typ
+    
+    
+    
+    /**
+     * @return Returns the changeDate.
+     */
+    public String getChangeDate() {
+        return changeDate;
+    }
+    /**
+     * @param changeDate The changeDate to set.
+     */
+    public void setChangeDate(String changeDate) {
+        this.changeDate = changeDate;
+    }
+    /**
+     * @return Returns the changeUser.
+     */
+    public String getChangeUser() {
+        return changeUserId;
+    }
+    /**
+     * @param changeUser The changeUser to set.
+     */
+    public void setChangeUser(String changeUserId) {
+        this.changeUserId = changeUserId;
+    }
+    /**
+     * @return Returns the createDate.
+     */
+    public String getCreateDate() {
+        return createDate;
+    }
+    /**
+     * @param createDate The createDate to set.
+     */
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+    /**
+     * @return Returns the createUser.
+     */
+    public String getCreateUser() {
+        return createUserId;
+    }
+    /**
+     * @param createUser The createUser to set.
+     */
+    public void setCreateUser(String createUserId) {
+        this.createUserId = createUserId;
+    }
+    /**
+     * @return Returns the description.
+     */
+    public String getDescription() {
+        return description;
+    }
+    /**
+     * @param description The description to set.
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    /**
+     * @return Returns the typ.
+     */
+    public String getTyp() {
+        return typ;
+    }
+    /**
+     * @param typ The typ to set.
+     */
+    public void setTyp(String typ) {
+        this.typ = typ;
+    }
+	
+}//class Typen
---
+++ dalieweb/JavaSource/database/getDatabase/DataSetTypen.java
@@ -0,0 +1,50 @@
+/*
+ * Created of DV0101 on 16.11.2009 12:31:14
+ *
+ * Filename	  DataSetTypen.java
+ */
+package database.getDatabase;
+
+import java.util.Vector;
+
+import database.Database;
+import database.dateien.Typen;
+
+/**
+ * Class description goes here.
+ *
+ * @version 	1.00 16.11.2009 um 12:31:14
+ * @author 		DV0101
+ * <br><br>
+ * private   = "false" nur sichtbar innerhalb der Klasse<br>
+ * ohne      = 	       sichtbar innerhalb Package<br>
+ * protected = "false" sichtbar innerhalb Package und abgeleiteten Klassen<br>
+ * public    = "false" sichtbar ausserhalb<br>
+ */
+public class DataSetTypen {
+	
+	/**
+     * <b>Typ aus Datenbank Tabelle Typ</b>
+     * <br><b>chain:Key String typId, String sprachId</b>
+     * <br><br><b>public static</b><br>
+     * @param
+     * <ul>
+     * <li>Database dbConn</li>
+     * <li>String typ = (U)seradresse </li>
+     * <li>String sprachId = DE</li>
+     * </ul>
+     * @return
+     * <ul>
+     * <li>Typ typ</li>
+     * </ul>
+     */
+    public static synchronized Typen chain(Database dbConn,String typId,String sprachId) throws Exception {
+    	Vector rows = dbConn.executeQuery("select typId, description, createUser, createDate, changeUser, changeDate from "+dbConn.getDbSchema()+".typ " +
+                "where typId = '"+typId+"' " +
+        		"and sprachId = '"+sprachId+"'");
+        	if(rows.size() == 0)
+        	    throw new Exception("Record not Found");
+        	return new Typen((Vector)rows.elementAt(0));            
+    }//getTypOfDokument
+	
+}//class DataSetTypen
