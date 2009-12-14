44b867ea7ac7d068cc42894fb2d9c1b67b613173
island.sommer <island.sommer@gmx.de>
2009-12-14 13:40:43 +0000

    Initial import.

---
+++ dalieweb/JavaSource/selinas/servlet/Selinas003Upload.java
@@ -0,0 +1,59 @@
+package selinas.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.Servlet;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class Selinas003Upload extends HttpServlet implements Servlet {
+	
+	 /** perform for both HTTP <code>GET</code> and <code>POST</code> methods  */
+	protected void perForm(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+	    
+		HttpSession session = request.getSession();
+		String nextPage = "/selinas/selinas001.jsp";
+		
+ 		try {
+ 	        RequestDispatcher displogin =  request.getRequestDispatcher("LogOnCheck");
+      		displogin.include(request, response);
+      		RequestDispatcher upload = request.getRequestDispatcher("UploadServlet");
+      		upload.include(request, response);
+ 		}catch (ServletException se) {
+ 		   performForward(nextPage,request,response);//Login 
+     	}//catch ServletException
+ 		performForward("/selinas/selinas003.jsp", request, response);
+	}//perForm
+    
+	/** handle the HTTP <code>GET</code> method */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+	    perForm(request,response);
+	}//doGet
+	/** handle the HTTP <code>POST</code> method */	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    perForm(request,response);
+	}//doPost
+	
+	/** handle the next step */
+	protected void performForward(String destination,HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {	
+		ServletContext context = getServletContext();
+		RequestDispatcher dispatcher = context.getRequestDispatcher(destination);
+		dispatcher.forward(request, response);
+	}//performForward
+	
+	public Selinas003Upload(){
+		super();
+	}//Selinas003Upload
+	
+	/**@return a short description of this Servlet */
+	public String getServletInfo() {
+	    return "get Upload of Selinas";
+	}//getServletInfo
+	
+
+}//class DokumentUploadServlet
