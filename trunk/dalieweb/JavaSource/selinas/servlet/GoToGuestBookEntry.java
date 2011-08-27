15753ceae7654752a87fdeaf0acca510c2fbee33
dalieStuttgart <dalieStuttgart@0d73a630-af78-11de-9d1a-45ab25924541>
2011-08-27 07:53:33 +0000

    Project for Lightbox for Guestbook

--- trunk/dalieweb/JavaSource/selinas/servlet/GoToGuestBookEntry.java
+++ trunk/dalieweb/JavaSource/selinas/servlet/GoToGuestBookEntry.java
@@ -3,9 +3,9 @@ package selinas.servlet;
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.Servlet;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,10 +14,8 @@ import javax.servlet.http.HttpSession;
 /**
  * Servlet implementation class GoGuestBook
  */
-@WebServlet(name = "GoToGuestBookEntry", description = "get Response of GuestBook", urlPatterns = { "/GoToGuestBookEntry" })
-public class GoToGuestBookEntry extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+public class GoToGuestBookEntry extends HttpServlet implements Servlet {
+	   
     
     /** perform for both HTTP <code>GET</code> and <code>POST</code> methods  */
 	protected void perForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
