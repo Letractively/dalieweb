3375e45c5d03e1256bc7bf72e62d5d6d75aa292d
island.sommer <island.sommer@gmx.de>
2011-12-03 12:11:40 +0000

    pattern: Servlet/JSP/JSTL adjustment for checkstyle

--- dalieweb/JavaSource/dalieweb/beehive/servlet/GoToServlet001.java
+++ dalieweb/JavaSource/dalieweb/beehive/servlet/GoToServlet001.java
@@ -1,5 +1,7 @@
 package dalieweb.beehive.servlet;
-
+/*
+* Created on 03.12.2011 by dalie
+*/
 import java.io.IOException;
 import java.util.Enumeration;
 import java.util.HashMap;
@@ -16,26 +18,37 @@ import javax.servlet.http.HttpSession;
 import dalieweb.beehive.bean.Address;
 
 /**
- * Servlet implementation class GoToServlet001
+ * Servlet implementation class GoToServlet001.
  */
 public class GoToServlet001 extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+
+	/** Constant to usage  for Request Attribute. */
     private static final String BEAN_FOR_ADDRESS = "Address";
-	
-	 /** perform for both HTTP <code>GET</code> and <code>POST</code> methods  */
-    protected void perForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    /**
+     * perform for both HTTP <code>GET</code>
+     * and <code>POST</code> methods.
+     *
+     * @param request The servlet container creates an HttpServletRequest object
+     * @param response The servlet container creates an HttpServletResponse
+     *
+     * @throws ServletException Defines a general exception
+     * @throws IOException Signals that an I/O exception
+     * **/
+    private void perForm(final HttpServletRequest request,
+    		final HttpServletResponse response)
+    				throws ServletException , IOException {
          HttpSession session = request.getSession(false);
-         if (session == null){
+
+         if (session == null) {
              return;
          }
-         
-         Address address = (Address)request.getAttribute(BEAN_FOR_ADDRESS);
+
+         Address address = (Address) request.getAttribute(BEAN_FOR_ADDRESS);
          if (address == null) {
         	 address = new Address();
          }
-         
-         
-         //System.out.println(address.toString());
+
          Map<String, String> mParameter = new HashMap<String, String>();
          Enumeration<?> names = request.getParameterNames();
          while (names.hasMoreElements()) {
@@ -43,7 +56,8 @@ public class GoToServlet001 extends HttpServlet {
            mParameter.put(name, request.getParameter(name));
          }
          if (mParameter.containsKey(Address.ADDRESS_FIRST_NAME)) {
-        	 address.setFirstName(mParameter.get(Address.ADDRESS_FIRST_NAME));
+        	 address.setFirstName(
+        			 mParameter.get(Address.ADDRESS_FIRST_NAME));
          }
          if (mParameter.containsKey(Address.ADDRESS_LAST_NAME)) {
         	 address.setLastName(mParameter.get(Address.ADDRESS_LAST_NAME));
@@ -58,46 +72,69 @@ public class GoToServlet001 extends HttpServlet {
         	 address.setCity(mParameter.get(Address.ADDRESS_CITY));
          }
          if (mParameter.containsKey(Address.ADDRESS_POSTAL_CODE)) {
-        	 address.setPostalCode(mParameter.get(Address.ADDRESS_POSTAL_CODE));
+        	 address.setPostalCode(
+        			 mParameter.get(Address.ADDRESS_POSTAL_CODE));
          }
          if (mParameter.containsKey(Address.ADDRESS_COUNTRY)) {
         	 address.setCountry(mParameter.get(Address.ADDRESS_COUNTRY));
          }
-         
+
          if (mParameter.containsKey(Address.ADDRESS_SUBMITTED)) {
         	 address.isValidate();
          }
-         
+
          request.setAttribute(BEAN_FOR_ADDRESS, address);
-         
+
          String nextPage = "/17_servlets/Servlets001.jsp";
-         performForward(nextPage,request,response);//JSP- User Login
-    }//perForm
-    
-	   /** handle the next step */
-    protected void performForward(String destination,HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {    
-         ServletContext context = getServletContext();
-         RequestDispatcher dispatcher = context.getRequestDispatcher(destination);
-         dispatcher.forward(request, response);
-    }//performForward
-    
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, 
-	 * HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, 
-			HttpServletResponse response) throws ServletException, IOException {
-		 perForm(request,response);
-	}
+         performForward(nextPage, request, response); /* forward to next */
+    } /* perForm */
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, 
-	 * HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, 
-			HttpServletResponse response) throws ServletException, IOException {
-		 perForm(request,response);
-	}
+	* handle the next step.
+	* @param destination next Task
+    * @param request The servlet container creates an HttpServletRequest object
+    * @param response The servlet container creates an HttpServletResponse
+	*
+	* @throws ServletException @see ServletException
+	* @throws IOException test
+	**/
+    private void performForward(final String destination, 
+    		final HttpServletRequest request, 
+    		final HttpServletResponse response)
+    				throws ServletException, IOException {
+
+		ServletContext context = getServletContext();
+		RequestDispatcher dispatcher = context
+				.getRequestDispatcher(destination);
+		
+		dispatcher.forward(request, response);
+	} /* performForward */
+
+    /**
+     * @param request The servlet container creates an HttpServletRequest object
+     * @param response The servlet container creates an HttpServletResponse
+     *
+     * @throws ServletException Defines a general exception
+     * @throws IOException Signals that an I/O exception
+     * **/
+	protected final void doGet(final HttpServletRequest request,
+			final HttpServletResponse response)
+					throws ServletException, IOException {
+		perForm(request, response);
+	} /* doGet */
+
+	/**
+	 * @param request The servlet container creates an HttpServletRequest object
+     * @param response The servlet container creates an HttpServletResponse
+     *
+     * @throws ServletException Defines a general exception
+     * @throws IOException Signals that an I/O exception
+     * **/
+	protected final void doPost(final HttpServletRequest request, 
+			final HttpServletResponse response) 
+					throws ServletException, IOException {
+		 perForm(request, response);
+	} /* doPost */
 
 	/**
 	 * @see HttpServlet#HttpServlet()
