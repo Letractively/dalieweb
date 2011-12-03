6585311628ffd964ff2d20633add788d77e814a6
dalieStuttgart <dalieStuttgart@googlemail.com>
2011-12-03 10:05:00 +0000

    pattern: Servlet/JSP/JSTL

--- dalieweb/JavaSource/dalieweb/beehive/servlet/GoToServlet001.java
+++ dalieweb/JavaSource/dalieweb/beehive/servlet/GoToServlet001.java
@@ -13,15 +13,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import dalieweb.beehive.bean.BeanForServlet001;
+import dalieweb.beehive.bean.Address;
 
 /**
  * Servlet implementation class GoToServlet001
  */
 public class GoToServlet001 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private static final String BEAN_FOR_SERVLET_001 = "BeanForServlet001";
-    
+    private static final String BEAN_FOR_ADDRESS = "Address";
+	
 	 /** perform for both HTTP <code>GET</code> and <code>POST</code> methods  */
     protected void perForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
          HttpSession session = request.getSession(false);
@@ -29,31 +29,47 @@ public class GoToServlet001 extends HttpServlet {
              return;
          }
          
-         BeanForServlet001 beanFS001 = (BeanForServlet001)request.getAttribute(BEAN_FOR_SERVLET_001);
-         if (beanFS001 == null) {
-        	 beanFS001 = new BeanForServlet001();			
+         Address address = (Address)request.getAttribute(BEAN_FOR_ADDRESS);
+         if (address == null) {
+        	 address = new Address();
          }
          
+         
+         //System.out.println(address.toString());
          Map<String, String> mParameter = new HashMap<String, String>();
          Enumeration<?> names = request.getParameterNames();
          while (names.hasMoreElements()) {
            String name = (String) names.nextElement();
            mParameter.put(name, request.getParameter(name));
          }
-         if (mParameter.containsKey("NameOFWebForm_Name")) {
-        	 if (mParameter.get("NameOFWebForm_Name").length() == 0) {
-        		 beanFS001.addMessage("Name is failed");
-        	 }
-			beanFS001.setName(mParameter.get("NameOFWebForm_Name"));
+         if (mParameter.containsKey(Address.ADDRESS_FIRST_NAME)) {
+        	 address.setFirstName(mParameter.get(Address.ADDRESS_FIRST_NAME));
          }
-         if (mParameter.containsKey("NameOFWebForm_FirstName")) {
-        	 if (mParameter.get("NameOFWebForm_FirstName").length() == 0) {
-        		 beanFS001.addMessage("First Name is failed");
-        	 }
-        	beanFS001.setFirstName(mParameter.get("NameOFWebForm_FirstName"));
+         if (mParameter.containsKey(Address.ADDRESS_LAST_NAME)) {
+        	 address.setLastName(mParameter.get(Address.ADDRESS_LAST_NAME));
+         }
+         if (mParameter.containsKey(Address.ADDRESS_STREET_1)) {
+        	 address.setStreet1(mParameter.get(Address.ADDRESS_STREET_1));
+         }
+         if (mParameter.containsKey(Address.ADDRESS_STREET_2)) {
+        	 address.setStreet2(mParameter.get(Address.ADDRESS_STREET_2));
+         }
+         if (mParameter.containsKey(Address.ADDRESS_CITY)) {
+        	 address.setCity(mParameter.get(Address.ADDRESS_CITY));
+         }
+         if (mParameter.containsKey(Address.ADDRESS_POSTAL_CODE)) {
+        	 address.setPostalCode(mParameter.get(Address.ADDRESS_POSTAL_CODE));
+         }
+         if (mParameter.containsKey(Address.ADDRESS_COUNTRY)) {
+        	 address.setCountry(mParameter.get(Address.ADDRESS_COUNTRY));
          }
          
-         request.setAttribute(BEAN_FOR_SERVLET_001, beanFS001);
+         if (mParameter.containsKey(Address.ADDRESS_SUBMITTED)) {
+        	 address.isValidate();
+         }
+         
+         request.setAttribute(BEAN_FOR_ADDRESS, address);
+         
          String nextPage = "/17_servlets/Servlets001.jsp";
          performForward(nextPage,request,response);//JSP- User Login
     }//perForm
