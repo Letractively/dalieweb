56a18b90b2501d3988be1fb607743c74a6bbddb6
dalieStuttgart@googlemail.com <dalieStuttgart@googlemail.com@0d73a630-af78-11de-9d1a-45ab25924541>
2011-12-04 17:21:13 +0000

    Project: nice Lightboxes to dalieweb

--- trunk/dalieweb/WebContent/script/dalieSite.js
+++ trunk/dalieweb/WebContent/script/dalieSite.js
@@ -8,21 +8,45 @@ function bindLinksWithAjaxContent() {
 		
 		if (getCustomTargetContainer != "") {
 		
-			alert(getCustomTargetContainer);
+			globalGetAjaxContent({
+				currentElement	:jQuery(this),
+				formSubmit		:jQuery(this).parents("forms"),
+				targetContainer	:"#" + jQuery(this).parents("target")
+			});
 		} else {
-			alert(getCustomTargetContainer);
+			globalGetAjaxContent({
+				currentElement	:jQuery(this),
+				formSubmit		:jQuery(this).parents("forms")
+			});
 		}
-		
 		return false;
 	});
 	
-}
-/* *** bindLinksWithAjaxContent - END *** */
+} /* *** bindLinksWithAjaxContent - END *** */
 
+/* *** globalGetAjaxContent - BEGINN */
+function globalGetAjaxContent(settings) {
+	
+	settings = jQuery.extend({//default settings
+		currentElement				:	null,
+		type						:	"GET",
+		formSubmit					:	null,
+		url							:	null,
+		data						:	null,
+		targetContainer				:	"targetContainerForAjaxContent",
+		nameOfLightbox				:	"#lightbox_content",
+		nameOfAjaxLoader			:	"#ajaxLoader",
+		doNotLoadContentInLightbox	: false,
+		onComplete					: function(){}
+	}, settings || {});
+	
 
-/* ######################################## DOCUMENT READY - BEGIN ######################################## */
+} /* *** globalGetAjaxContent - END *** */
+
+/* #################### DOCUMENT READY - BEGIN ############################# */
 jQuery(document).ready(function(){
 	/* NOTE: initialize all jQuery functionalities here */
+	
 	bindLinksWithAjaxContent();
 });
-/* ######################################## DOCUMENT READY - END ######################################## */
+/* #################### DOCUMENT READY - END    ############################ */
